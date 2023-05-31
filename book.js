5fbb76754e5f24bf5b003234f6b9ed46832c154b
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-01 00:34:15 +0600

    up

--- book.js
+++ book.js
@@ -5,9 +5,8 @@ export class Book {
         this.subtitle = data.subtitle;
         this.authors = data.authors;
         this.imageSrc = data.image;
-        this.readerUrl = '';
+        this.readerId = '';
         this.returnDate = null;
-
         //Создаю элемент книги
         this.view = document.createElement('div');
         this.view.id = this.title;
@@ -16,37 +15,47 @@ export class Book {
         let img = document.createElement('img');
         img.src = this.imageSrc;
         //Вставляю название и описание
-        let title = document.createElement('h2');
+        let title = document.createElement('p');
         title.textContent = this.title;
-        let subtitle = document.createElement('p');
-        subtitle.textContent = this.subtitle;
-        let onStock = document.createElement('p');
-        if (this.readerUrl == '') {
-            onStock.textContent = "Книга доступна"
-        } else onStock.textContent = "Книга не доступна"
+        let inStock = document.createElement('p');
+        inStock.className = 'in-stock'
+        if(this.readerId === ''){
+            inStock.textContent = "Доступна";
+        } else inStock.textContent = "Недоступна";
         //Все вставляю
         this.view.append(img);
         this.view.append(title);
-        this.view.append(subtitle);
-        this.view.append(onStock);
+        this.view.append(inStock);
     }
 
-    giveOutBook(reader) {
-        this.readerUrl = reader;
-        this.returnDate = new Date(); // + 7 дней
-        this.returnDate.setDate(getDate)
+    //Выдать книгу указанному Читателю через Id 
+    giveOutBook(readerId) {
+        //Принимаю в метод readerId айди читателя и  устанавливаю его в свойство в книге
+        this.readerId = readerId;
+        //Книга выдаетс на семь дней от дня когда ее взяли
+        let today = new Date(); // + 7 дней
+        this.returnDate.setDate(today.getDate() + 7);
+        //Синхронизирую, для того чтобы убрать пометку что она Доступна в диве, ведь ее забрали
         this.sync();
     }
 
+    //Возвращение книги
     returnBook() {
+        //Обнуляю все данные и через синхронизацию пометку Недоступна меняю на Доступно, т.к. книгу Вернули
         this.readerUrl = '';
         this.returnDate = null;
-        this.onStock = true;
         this.sync();
     }
 
     sync() {
-        returnMark.textContent = this.onStock;
+        //Если в ридерАйди пусто, книга без читателя и Доступна
+        if(this.readerId === ''){
+            inStock.textContent = "Доступна";
+        } else inStock.textContent = "Недоступна"; //Если есть свойство указывающее на читателя, то книга Недоступна
+    }
+
+    bookDetails(){
+
     }
 
 }
--- main.js
+++ main.js
@@ -2,79 +2,18 @@ import { Book } from "./book.js";
 import { Reader } from './reader.js'
 const log = console.log;
 
-const url = 'https://www.dbooks.org/api/recent';
-// fetchData(url);
-
-// async function fetchData(url){
-//     let response =  fetch(url).then(response => response.json()).then(response => log(response));
-// }
-
-let data = {
-    fullName: ''
-    , address: ''
-    , phoneNum: ''
-    , image: ''
-};
-const xhr = new XMLHttpRequest();
-
-
-xhrTrendBooks.onreadystatechange = (ev) => {
-    if(xhrTrendBooks.status === 200){
-        if(xhrTrendBooks.readyState ===xhrTrendBooks.DONE){
-            let result = JSON.parse(xhrTrendBooks.response);
-            log(result.books);
-
-            let divShelf = document.querySelector('.trend-books');
-
-            for(let i=0 ; i<10; i++){
-                let book = new Book(result.books[i]);
-
-                
-                divShelf.append(book.view);
-            }
-        }
-    }
-}
-
-xhrTrendBooks.open('get', url, true);
-xhrTrendBooks.send();
-
-let inputSearch = document.querySelector('.search-book');
-
-const xhrSearch = new XMLHttpRequest();
-
-inputSearch.addEventListener('keyup', () => {
-    let text = inputSearch.value;
-    log(text);
-
-    xhrSearch.open('get', `https://www.dbooks.org/api/search/${text}`,  true);
-    xhrSearch.send();
-});
-
-xhrSearch.onreadystatechange = (ev) => {
-    if(xhrSearch.status === 200){
-        if(xhrTrenxhrSearchdBooks.readyState ===xhrSearch.DONE){
-            let result = JSON.parse(xhrSearch.response);
-            log(result.books);
-
-            let divResult = document.querySelector('.search-result');
-
-            for(let x of result.books){
-                let book = new Book(x);
-
-                divResult.append(book.view);
-            }
-        }
-    }
-}
-
-
 class Library {
     #userData = [];
     #giveOutBookData = [];
     constructor(){
-        
+        this.newBooksUrl = 'https://www.dbooks.org/api/recent';
+        this.searchUrl = 'https://www.dbooks.org/api/search/'
+        this.trendyBooksShelf = null;
+        this.searchResult = null;
+    }
 
+    main(){
+        this.newBooks();
     }
 
     newUser(){
@@ -85,8 +24,58 @@ class Library {
     return(title, authors){
         let book = 
         this.#giveOutBookData.slice
-
     }
 
+    newBooks(divShelf){
+        this.trendyBooksShelf = divShelf;
+        let response =  fetch(this.newBooksUrl)
+                        .then(response => response.json())
+                        .then(response => {
+                            log(response.books);
+                            for(let i = 0; i < 10; i++){
+                              let book = new Book(response.books[i]);
+                              this.trendyBooksShelf.append(book.view);
+                            }
+                        });
+    }
 
+    search(text, divResult){
+        this.searchResult = divResult;
+        this.searchResult.innerHTML = '';
+        let response = fetch(this.searchUrl + text)
+                        .then(response => response.json())
+                        .then(response => {
+                            log(response.books)
+                            for(let i = 0; i < 10; i++){
+                                let book = new Book(response.books[i]);
+                                this.searchResult.append(book.view);
+                            }
+                        });
+    }
 }
+
+// const url = 'https://www.dbooks.org/api/recent';
+
+// let response =  fetch(url).then(response => response.json()).then(response => log(response));
+
+let data = {
+    fullName: ''
+    , address: ''
+    , phoneNum: ''
+    , image: ''
+};
+
+//Создаю библиотеку по классу Library
+const library = new Library();
+//Получаю див куда будут заноситься новинки книг через метод library.newBooks(divShelf);
+let divShelf = document.querySelector('.trend-books');
+library.newBooks(divShelf);
+//Подключаю поиск на кнопку searchButton через метод из библиотеки library.search(text, divResult)
+// И ввожу в этот метод поиска текст из инпута
+let inputSearch = document.querySelector('#search-book');
+let searchButton = document.querySelector('#search');
+let divResult = document.querySelector('.search-result');
+searchButton.addEventListener('click', () => {
+    let text = inputSearch.value;
+    library.search(text, divResult);
+})
