aee6446659893f43b92c7caee8c6e2a989903bbb
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-05-31 17:37:42 +0600

    up

--- book.js
+++ book.js
@@ -1,4 +1,52 @@
 export class Book {
-    
+    constructor(data){
+        this.id = data.id;
+        this.title = data.title;
+        this.subtitle = data.subtitle;
+        this.authors = data.authors;
+        this.imageSrc = data.image;
+        this.readerUrl = '';
+        this.returnDate = null;
+        this.onStock = true;
+
+        //Создаю элемент книги
+        this.view = document.createElement('div');
+        this.view.id = this.title;
+        //вставляю картинку
+        let img = document.createElement('img');
+        img.src = this.imageSrc;
+        //Вставляю название и описание
+        let title = document.createElement('h2');
+        title.textContent = this.title;
+        let subtitle = document.createElement('p');
+        subtitle.textContent = this.subtitle;
+        let onStock = document.createElement('p');
+        returnMark.textContent = this.onStock;
+        //Все вставляю
+        this.view.append(img);
+        this.view.append(title);
+        this.view.append(subtitle);
+        this.view.append(onStock);
+    }
+
+    giveOutBook(reader){
+        this.readerUrl = reader;
+        this.returnDate = new Date(); // + 7 дней
+        this.returnDate.setDate(getDate)
+        this.onStock = false;
+        this.sync();
+    }
+
+    returnBook(){
+        this.readerUrl = '';
+        this.returnDate = null;
+        this.onStock = true;
+        this.sync();
+    }
+
+    sync(){
+        returnMark.textContent = this.onStock;
+    }
 
 }
+
